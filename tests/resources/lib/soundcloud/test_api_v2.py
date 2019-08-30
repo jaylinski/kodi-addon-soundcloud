872ca5da189b21e7796ed4fc7343549aaea3dd25
Jakob Linskeseder <jakob.linskeseder@gmail.com>
2019-08-30 15:29:21 +0200

    Use thumbnails with higher resolution
    The logic (including the regex) is directly derived from the
    SoundCloud front-end code.
    
    Resolves #22

--- tests/resources/lib/soundcloud/test_api_v2.py
+++ tests/resources/lib/soundcloud/test_api_v2.py
@@ -41,9 +41,44 @@ class ApiV2TestCase(TestCase):
         self.assertEqual(res.items[0].label, "Deadmau5 - Raise Your Weapon (Noisia Remix)")
         self.assertEqual(res.items[0].info["artist"], "NOISIA")
         self.assertEqual(res.items[0].media, "https://api-v2.soundcloud.com/media/soundcloud:tracks:15784497/580ad806-b3ab-440f-adbe-c12a83258a37/stream/progressive")
+        self.assertEqual(res.items[0].thumb, "https://i1.sndcdn.com/artworks-000007527658-smjpzh-t500x500.jpg")
+
         self.assertEqual(res.items[1].label, "Labrinth ft. Tinie Tempah - Earthquake (Noisia Remix)")
         self.assertEqual(res.items[1].info["artist"], "NOISIA")
         self.assertEqual(res.items[1].media, "https://api-v2.soundcloud.com/media/soundcloud:tracks:23547065/e7846551-5c8e-4b93-b4f0-f94bfa7b1275/stream/progressive")
+        self.assertEqual(res.items[1].thumb, "https://i1.sndcdn.com/artworks-000011681052-n1a6w6-t500x500.jpg")
+
+    def test_search_playlists(self):
+        with open("./tests/mocks/api_v2_search_playlists_without_albums.json") as f:
+            mock_data = f.read()
+
+        self.api._do_request = Mock(return_value=json.loads(mock_data))
+
+        res = self.api.search("foo")
+
+        self.assertEqual(res.items[0].label, "Noisia")
+        self.assertEqual(res.items[0].info["artist"], "Sebastian Morad")
+        self.assertEqual(res.items[0].thumb, "https://i1.sndcdn.com/artworks-000498621510-fk1ovg-t500x500.jpg")
+
+        self.assertEqual(res.items[1].label, "NOISIA")
+        self.assertEqual(res.items[1].info["artist"], "Samuel Harris")
+        self.assertEqual(res.items[1].thumb, None)
+
+    def test_search_users(self):
+        with open("./tests/mocks/api_v2_search_users.json") as f:
+            mock_data = f.read()
+
+        self.api._do_request = Mock(return_value=json.loads(mock_data))
+
+        res = self.api.search("foo")
+
+        self.assertEqual(res.items[0].label, "NOISIA")
+        self.assertEqual(res.items[0].label2, "Outer Edges")
+        self.assertEqual(res.items[0].thumb, "https://i1.sndcdn.com/avatars-000451809714-n5njwk-t500x500.jpg")
+
+        self.assertEqual(res.items[1].label, "Noisia Radio")
+        self.assertEqual(res.items[1].label2, "Noisia  Radio")
+        self.assertEqual(res.items[1].thumb, "https://i1.sndcdn.com/avatars-000559848966-7tof1c-t500x500.jpg")
 
     def test_resolve_id(self):
         with open("./tests/mocks/api_v2_tracks.json") as f:
