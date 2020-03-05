1d9ba4cda6eaf1e91a690968d24977b7aed66a26
Jakob Linskeseder <jakob.linskeseder@gmail.com>
2020-03-05 21:02:21 +0100

    Exclude Python cache files from build

--- scripts/build.py
+++ scripts/build.py
@@ -6,7 +6,7 @@ import shutil
 src_dir = "."
 build_dir = "build"
 addon_name = "plugin.audio.soundcloud"
-ignore = shutil.ignore_patterns(".*", "scripts", "tests", "venv", "Pipfile*")
+ignore = shutil.ignore_patterns(".*", "scripts", "tests", "venv", "Pipfile*", "__pycache__")
 
 # Clean up
 if os.path.exists(build_dir):
